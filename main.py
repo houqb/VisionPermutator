ca677d0d662fee4de5c3cdcb636aeeca8d9839e6
zihangJiang <32448880+zihangJiang@users.noreply.github.com>
2021-06-23 21:38:46 +0800

    init

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Copyright (c) [2012]-[2021] Shanghai Yitu Technology Co., Ltd.
-#
-# This source code is licensed under the Clear BSD License
-# LICENSE file in the root directory of this file
-# All rights reserved.
 """
 ViP training and evaluating script
 This script is modified from pytorch-image-models by Ross Wightman (https://github.com/rwightman/pytorch-image-models/)
